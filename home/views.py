ab5794fa3f4b4c1f9b5d0f5dc253502363ee600e
lucascardlima <142846329+lucascardlima@users.noreply.github.com>
2024-10-26 15:15:34 -0300

    .

--- home/views.py
+++ home/views.py
@@ -3,15 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django .http import HttpResponse
 
+
 def index(request):
     return render(request, 'index.html')
 
 
 def sobre(request):
-    return HttpResponse('<h1>Sistema 1.0 desenvolvido por mesmo<h1>')
+    return render(request, 'sobre.html')
 
 def contato(request):
-    return HttpResponse('<h1>Esta é a página de contato<h1>')
+    return render(request, 'contato.html')
 
 def ajuda(request):
     return HttpResponse('<h1>Esta é a página de ajuda<h1>')
