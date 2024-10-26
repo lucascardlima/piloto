2bf49a3ae5d2abf4225b1db33fae5835085d0cff
lucascardlima <142846329+lucascardlima@users.noreply.github.com>
2024-10-26 15:06:19 -0300

    atividade

---
+++ home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django .http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def contato(request):
+    return render(request, 'conato.html')
+
+def ajuda(request):
+    return HttpResponse('<h1>Esta é a página de ajuda<h1>')
